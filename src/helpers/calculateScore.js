4601a2ff6b75e85018e4591e824ccb59f5f2f602
philipmarkose <129611831+philipmarkose@users.noreply.github.com>
2025-05-02 16:53:07 -0700

    remove console log

--- src/helpers/calculateScore.js
+++ src/helpers/calculateScore.js
@@ -3,8 +3,6 @@ import { system_prompt } from '../constants/systemPrompt.js';
 import { config } from 'dotenv';
 config();
 
-console.log(process.env.GEMINI_API_KEY)
-
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function calculateScore(mysteryWord, guesses) {
